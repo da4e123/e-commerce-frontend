0164371cd5b68060d286cf3a5377c4412a18c7be
da4e123 <dalibor.blazhevski@students.finki.ukim.mk>
2025-01-09 00:39:56 +0100

    Updated page

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/shopContext';
-import { ShopContext } from '../context/shopContext';
 import Title from '../components/Title';
 
 const Orders = () => {

54b3d54b3f4a029a756858a829db60d73ce313e8
da4e123 <dalibor.blazhevski@students.finki.ukim.mk>
2025-01-09 00:34:17 +0100

    Updated some pages

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '/src/context/shopContext'; 
+import { ShopContext } from '../context/shopContext'; 
 import assets from '../assets/assets';
 import Title from '../components/Title';
 import ProductItem from '../components/ProductItem';
